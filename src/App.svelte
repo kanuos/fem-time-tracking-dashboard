c04b87e91f90cca9b0a0f13db8e35886cb3089a8
kanuos <sounakmukherjee@ymail.com>
2023-01-25 11:15:50 -0500

    setup - Install Svelte using Vite build tool - Install and configure Tailwind.css - Clean up boilerplate code - Add a welcome screen

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<main
+  class="bg-gradient-to-br from-green-400 to-blue-500 min-h-screen grid place-items-center"
+>
+  <h1
+    class="text-9xl break-words font-thin text-center text-black drop-shadow-md"
+  >
+    Hello World!
+  </h1>
+</main>
